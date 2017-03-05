7af2f08f9bea20b5112b75c91d0ea9a107f877f1
Matt <matt.leonard.22@gmail.com>
2017-03-05 14:24:44 -0800

    working on displaying blocks

--- js/HeatMap.js
+++ js/HeatMap.js
@@ -4,11 +4,11 @@ var xhr = new XMLHttpRequest();
 xhr.open('GET', 'https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/global-temperature.json');
 xhr.onload = function() {
   if (xhr.status === 200) {
-    var json = JSON.parse(xhr.response);
-    console.log(json);
+    var dataset = JSON.parse(xhr.response);
+    console.log(dataset);
 
     // Instantiate variables
-    var chartWidth = 800;
+    var chartWidth = 900;
     var chartHeight = 500;
     
 
@@ -18,6 +18,21 @@ xhr.onload = function() {
                 .attr('width', chartWidth)
                 .attr('height', chartHeight)
                 .attr('class', 'chartBackground')
+
+    // Append rects to svg
+    svg.selectAll('rect')
+       .data(dataset.monthlyVariance)
+       .enter()
+       .append('rect')
+       .attr('x', function(d, i) { return d.year - 1753 + 5 ; })
+       .attr('y', function(d) { return d.month * 30 - 30; })
+       .attr('width', '4px')
+       .attr('height', '30px')
+       .attr('fill', 'black')
+       .attr('stroke', 'black')
+       .append('title')
+       .text(function(d) { return d.month + " " + d.year });
+
   }
   else {
     // Handle error if xhr request fails
