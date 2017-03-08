1392987837ae546ab47fc255f9a3d924228dd932
Matt <matt.leonard.22@gmail.com>
2017-03-07 19:22:35 -0800

    Got rects laid out appropriately and color coded based on temperature differences

--- js/HeatMap.js
+++ js/HeatMap.js
@@ -8,8 +8,8 @@ xhr.onload = function() {
     console.log(dataset);
 
     // Instantiate variables
-    var chartWidth = 900;
-    var chartHeight = 500;
+    var chartWidth = 789;
+    var chartHeight = 420;
     
 
     // Append svg to page
@@ -24,12 +24,35 @@ xhr.onload = function() {
        .data(dataset.monthlyVariance)
        .enter()
        .append('rect')
-       .attr('x', function(d, i) { return d.year - 1753 + 5 ; })
-       .attr('y', function(d) { return d.month * 30 - 30; })
-       .attr('width', '4px')
-       .attr('height', '30px')
-       .attr('fill', 'black')
-       .attr('stroke', 'black')
+       .attr('x', function(d, i) { return (d.year - 1753) * 3})
+       .attr('y', function(d) { return d.month * 35 - 35; })
+       .attr('width', '3px')
+       .attr('height', '35px')
+       .attr('fill', function(d) {
+         if (d.variance < -3.0) {
+            // Temps 3 degrees below average are colored blue
+           return '#00bfff'
+         } else if (d.variance > -3.0 && d.variance < -1.0) {
+           // Temps between 1 and 3 degrees below average are colored pale green
+           return '#abfcab';
+         } else if (d.variance >= -1.0 && d.variance < -0.5) {
+           // Temps between -1 and -0.5 degrees below average are colored darker green
+           return '#61f961';
+         } else if (d.variance > -0.5 && d.variance < 0.5) {
+           // Temps close to average are colored pale yellow
+           return '#ffffb1';
+         } else if (d.variance > 0.5 && d.variance <= 1.0) {
+           // Temps between 0.5 and 1 degree above average are colored darker yellow
+           return '#ffff65';
+         } else if (d.variance > 1.0 && d.variance < 3.0) {
+           // Temps between 1 and 3 degrees above average are colored pale orange
+           return '#ffac14';
+         } else if (d.variance > 3.0) {
+           // Temps above 3 degrees above normal are colored dark orange
+           return '#c78000';
+         }
+        })
+       .attr('class', 'box')
        .append('title')
        .text(function(d) { return d.month + " " + d.year });
 
